e11055179f44e3625084539e307bcaec3bdf9cae
Henry Vilchez <henry.vilchez@acorde.com.ve>
2019-02-24 07:59:37 -0400

    Navegación por Tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LugarPage } from '../pages/lugar/lugar';
+import { TabsPage } from '../pages/tabs/tabs';
+import { PerfilPage } from '../pages/perfil/perfil';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LugarPage,
+    TabsPage,
+    PerfilPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LugarPage,
+    TabsPage,
+    PerfilPage
   ],
   providers: [
     StatusBar,
